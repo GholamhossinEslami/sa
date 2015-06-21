a21e95c7db00cc5fc5fb75d81b4023d8a1c78910
Gholamhossin Eslami <gholamhossin.eslami@pmart.ir>
2015-06-21 13:36:02 +0430

    new file:   mainwindows.cpp 	new file:   mainwindows.h 	modified:   sa.pro

---
+++ mainwindows.h
@@ -0,0 +1,17 @@
+#ifndef MAINWINDOWS_H
+#define MAINWINDOWS_H
+
+#include <QMainWindow>
+
+class MainWindows : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindows(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // MAINWINDOWS_H
